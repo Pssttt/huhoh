7b65ac0aa8495592f83a2e1558879262c34e0cc1
Waiyanheinoo <waiyanheinoo99@gmail.com>
2025-05-31 13:16:33 +0700

    feat: add translation model and controller
    This commit introduces the basic structure for handling
    translations, including a model for data access and a
    controller for request handling.

--- backend/src/models/translation.model.ts
+++ backend/src/models/translation.model.ts
@@ -1,8 +1,11 @@
+import type { Translation } from "../generated/prisma/client.js";
 import db from "../lib/db.js";
 
 const getTranslationById = async (id: string) => {
   const translation = await db.translation.findUnique({ where: { id } });
   return translation;
 };
+
+const createTranslation = async () => {};
 
 export { getTranslationById };
