a8b0ffba306d237e54b122b12017abe0cfdc3a87
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-05-31 12:54:14 +0700

    feat(landing): add hero section and reformat using prettier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import LandingPage from './pages/Landing/LandingPage'
 
 const App = () => {
-    return (
-        <div>
-            <LandingPage />
-        </div>
-    )
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  )
 }
 
 export default App
