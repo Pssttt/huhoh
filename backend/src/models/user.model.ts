d7f7e08036dd5bfa1bc16380c3781a6cbb357176
Bhone Thant Lwin <bhonethantlwin@Bhones-MacBook-Air.local>
2025-05-31 10:10:23 +0700

    feat(user.model.ts): add user model functions
    This commit adds the following functions to the user model:
    
    - `findByEmail`: Finds a user by email.
    - `findByUsername`: Finds a user by username.
    - `findById`: Finds a user by ID.
    - `createUser`: Creates a new user.
    - `getUserInfo`: Gets user info by ID.
    - `validatePassword`: Validates user password.
    
    These functions will be used to manage users in the application.

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,46 +1,53 @@
-import db from "../;
-const findByEmail = async (email : String) => {
-    const mail = await db.user.findUnique({
-        where : {email},
-    });
-    return mail;
+import db from "../lib/db.js";
+import { hash, compare } from "bcryptjs";
+const findByEmail = async (email: String) => {
+  const mail = await db.user.findUnique({
+    where: { email },
+  });
+  return mail;
 };
 
-const findByUsername = async (username : String) => {
-    const account = await db.user.findUnique({
-        where : {username},
-    });
-    return account;
+const findByUsername = async (username: String) => {
+  const account = await db.user.findUnique({
+    where: { username },
+  });
+  return account;
 };
 
-const findById = async (id : String)=>{
-    const user = await db.user.findUnique({
-        where : {id},
-    });
-    return user;
+const findById = async (id: String) => {
+  const user = await db.user.findUnique({
+    where: { id },
+  });
+  return user;
 };
 
-const createUser = async (userName : String , email : String, password : String) =>{
-    const hashedPassword = await hash (password,10);
-    const user = await db.user.create({
-        data :{
-            userName : userName,
-            email : email,
-            password : hashedPassword,
-        },
-    });
-    return user;
+const createUser = async (
+  userName: string,
+  email: string,
+  password: string
+) => {
+  const hashedPassword = await hash(password, 10);
+  const user = await db.user.create({
+    data: {
+      userName: userName,
+      email: email,
+      password: hashedPassword,
+    },
+  });
+  return user;
 };
 
-const getUserInfo = async (id : String) => {
-    const user = await db.user.findUnique({
-        where : {id},
-        select : {
-            username : true,
-            profilePic : true,
-        }
-    });
-    return user;
+const getUserInfo = async (id: String) => {
+  const user = await db.user.findUnique({
+    where: { id },
+    select: {
+      username: true,
+      profilePic: true,
+    },
+  });
+  return user;
 };
 
-con
+const validatePassword = async (input: String, hash: String) => {
+  return compare(input, hash);
+};
