4251459290cd8e8c731e30a9e03ccf820f92f1be
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-05-31 12:38:16 +0700

    demo Landing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import LandingPage from './pages/Landing/LandingPage'
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div>
+            <LandingPage />
+        </div>
+    )
 }
 
 export default App
